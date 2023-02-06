91af370f282cf130312b63e2e83c89370e3d3510
lashuki1995 <104417338+lashuki1995@users.noreply.github.com>
2023-02-06 11:23:31 +0400

    course

---
+++ coursehunter/index.js
@@ -0,0 +1,37 @@
+// const output = fizzBuzz(3)
+// console.log(output)
+
+// function fizzBuzz(input) {
+//   if (typeof(input) !== "number") return Nan
+//   if (input % 3 === 0 && input % 5 === 0) return "FizzBuzz"
+//   if (input % 3 === 0) return "Fizz"
+//   if (input % 5 === 0) return "Buzz"
+//   if (input % 3 !== 0 && input % 5 !== 0) return input
+// }
+// ####################   next      #######################
+// let a = 1
+// a > 2 ? alert("sworia") : alert("arasworia")
+
+// if (a > 2) {
+//   console.log("sworia")
+// }
+// else {
+//   console.log("arasworia")
+// }
+// #####################     next    ################################
+// let person = {
+//     name: 'lasha',
+//     lastName:'giogadze',
+//     age:27
+
+// }
+// person.name='lashuki'
+// let selection='name'
+// person[selection]='lashexa'
+// console.log(person) 
+// ######################       next     ###############################
+
+// function lasha(name , lastName){
+//     console.log(name+lastName)
+// }
+// lasha = ('lasha', 'giorgadze')
